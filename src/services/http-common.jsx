f294846c53ee29da2a99bc2b332f19b888edc0b7
SelinKarpuzcu <selin294284@gmail.com>
2024-12-27 19:46:14 +0300

    base url değiştirildi

--- src/services/http-common.jsx
+++ src/services/http-common.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "https://backend-git-main-selin-karpuzcu-projects.vercel.app/",
+  baseURL: "https://backend-git-main-selin-karpuzcu-projects.vercel.app/api",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
